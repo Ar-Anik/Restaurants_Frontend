bb276b63bd834cafe951f3965fb1574aa02449e5
Ar-Anik <67730427+Ar-Anik@users.noreply.github.com>
2021-05-15 11:29:59 +0600

    complete

--- js/script.js
+++ js/script.js
@@ -8,6 +8,22 @@ $(document).ready(function(){
     $(window).on('scroll load', function(){
         $('#menu-bar').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let id = $(this).attr('id');
+            let offset = $(this).offset().top - 200;
+
+            if(top > offset && top < offset + height)
+            {
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
     });
 
     $('.menu .list .btn').click(function(){
